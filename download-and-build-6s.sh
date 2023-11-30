f21aeb5a1cc1c31873cdadc74164c3ec9c7b5546
Kevin Wurster (Planet Corporate Activities) <kevin.wurster@planet.com>
2023-11-30 17:35:22 -0500

    REBASE ci

---
+++ download-and-build-6s.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+SIXS_DIR="6sv-2.1"
+
+mkdir -p "${SIXS_DIR}"
+cd "${SIXS_DIR}"
+wget https://github.com/ashiklom/isofit/releases/download/6sv-mirror/6sv-2.1.tar
+
+tar -xf 6sv-2.1.tar
+
+cp Makefile Makefile.bak
+sed -i -e 's/FFLAGS.*/& -std=legacy/' Makefile
+
+make
---
+++ download-and-unpack-sRTMnet.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+SRTMNET_DIR="sRTMnet_v100"
+SRTMNET_FILENAME="sRTMnet_v100.zip"
+
+
+# Just assume that if the target directory exists the data is downloaded and
+# properly unpacked.
+if [ -d "$SRTMNET_DIR" ]; then
+  echo "Directory already exists - nothing to do: ${SRTMNET_DIR}"
+  exit 0
+fi
+
+
+# Download sRTMnet file
+mkdir "$SRTMNET_DIR"
+wget \
+  --no-verbose \
+  --directory-prefix "$SRTMNET_DIR" \
+  "https://zenodo.org/record/4096627/files/${SRTMNET_FILENAME}"
+
+
+(
+  cd "${SRTMNET_DIR}"
+  unzip "${SRTMNET_DIR}"
+  rm -rf "__MACOSX"
+  rm "${SRTMNET_FILENAME}"
+)
