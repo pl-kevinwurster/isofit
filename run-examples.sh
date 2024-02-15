de3645262e4a6fd6d9d9d06f483b36d0d4dc0012
unbohn <nbohn.1988@googlemail.com>
2024-02-14 18:49:34 -0800

    Added run examples step to test-pip-package-ubuntu.yml.

---
+++ run-examples.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shellcheck disable=SC2164
+# run 20151026_SantaMonica example
+cd examples/20151026_SantaMonica
+bash run_examples.sh
+
+# run 20171108_Pasadena example
+cd examples/20171108_Pasadena
+bash run_example_modtran.sh
+python run_topoflux_example.py
+
+# run 20190806_ThermalIR example
+cd examples/20190806_ThermalIR
+python run_example_modtran_one.p
+
+# run profiling_cube/small example
+cd examples/profiling_cube
+ISOFIT_DEBUG=1 python run_profiling.py
