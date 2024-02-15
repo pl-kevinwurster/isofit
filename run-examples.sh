5b2b79542f354a9a12da6bfb63f428babd0770d2
unbohn <nbohn.1988@googlemail.com>
2024-02-14 20:12:25 -0800

    Fixed changing directory commands in run-examples.sh.

--- run-examples.sh
+++ run-examples.sh
@@ -16,14 +16,14 @@ cd examples/20151026_SantaMonica
 bash run_examples.sh
 
 # run 20171108_Pasadena example
-cd examples/20171108_Pasadena
+cd ../../examples/20171108_Pasadena
 bash run_example_modtran.sh
 python run_topoflux_example.py
 
 # run 20190806_ThermalIR example
-cd examples/20190806_ThermalIR
+cd ../../examples/20190806_ThermalIR
 python run_example_modtran_one.py
 
 # run profiling_cube/small example
-cd examples/profiling_cube
+cd ../../examples/profiling_cube
 ISOFIT_DEBUG=1 python run_profiling.py
